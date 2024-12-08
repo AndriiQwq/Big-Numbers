10d5aab3c835078c7522fd8dee890911f5d9ece2
AndriiQwq <xdokaniev@stuba.sk>
2024-12-08 20:59:32 +0100

    start

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Big_Numbers.hpp"
+
+int main(){
+    //std::string str = "12345678901234567890123456789012345678901234567890123456789012345678901234567890";
+
+    // BigInteger Big_Numbers();
+    // std::cout << Big_Numbers << std::endl;
+    BigInteger big_num("128900");
+    std::cout << big_num << '\n';
+}
